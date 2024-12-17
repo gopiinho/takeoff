b644a6a56767a9d4f38e56b1da04621efa4a1919
Gurpreet <gs109690@gmail.com>
2024-12-17 23:39:32 +0530

    chore: updated deploy script

--- script/DeployTokenFactory.s.sol
+++ script/DeployTokenFactory.s.sol
@@ -7,11 +7,17 @@ import {TokenFactory} from "../src/TokenFactory.sol";
 contract DeployTokenFactory is Script {
     TokenFactory public tokenFactory;
 
+    function setUp() public {}
+
     function run() external {
-        vm.startBroadcast();
+        uint256 privateKey = vm.envUint("DEV_KEY");
+        address account = vm.addr(privateKey);
+
+        vm.startBroadcast(account);
         tokenFactory = new TokenFactory();
         vm.stopBroadcast();
 
+        // Log the address of the deployed contract and the transaction hash
         console2.log("TokenFactory deployed to: ", address(tokenFactory));
     }
 }
